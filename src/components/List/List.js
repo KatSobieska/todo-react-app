57c028def0ed0823b32048ebb9b7fdc71ef08e91
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-19 13:42:10 +0200

    Move column functions into ColumnForm and add ColumnForm import

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,9 @@
 import styles from "./List.module.scss";
 import Column from "../Column/Column.js";
+import ColumnForm from '../ColumnForm/ColumnForm.js';
 import { useState } from "react";
 import shortid from 'shortid';
+
 
 const List = () => {
   const [columns, setColumns] = useState([
@@ -10,13 +12,9 @@ const List = () => {
     { id: 3, title: "Games", icon: "gamepad" },
   ]);
 
-  const handleSubmit = e => {
-	e.preventDefault();
-	setColumns([...columns, { id: shortid(), title: value }]);
-    setValue('');
+  const addColumn = newColumn => {
+    setColumns([...columns, { id: shortid(), title: newColumn.title }]);
 };
-
-const [value, setValue] = useState('');
 
   return (
     <div className={styles.list}>
@@ -33,10 +31,7 @@ const [value, setValue] = useState('');
           <Column key={column.id} title={column.title} icon={column.icon} />
         ))}
       </section>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
-        <button>Add column</button>
-      </form>
+      <ColumnForm action={addColumn} />
     </div>
   );
 };
