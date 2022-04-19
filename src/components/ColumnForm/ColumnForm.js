883c49ec64385b6d35931279eea6409e342f66d8
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-19 13:51:13 +0200

    Add new field for icon

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,21 +1,27 @@
-import styles from './ColumnForm.module.scss';
+import styles from "./ColumnForm.module.scss";
 import { useState } from "react";
 
-const ColumnForm = props => {
+const ColumnForm = (props) => {
+  const [value, setTitle] = useState("");
+  const [icon, setIcon] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.action({ title: value, icon: icon });
+    setTitle("");
+    setIcon("");
+  };
 
-    const [value, setValue] = useState('');
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.action({ title: value });
-        setValue('');
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
-        <button>Add column</button>
-      </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      Title: <input
+        type="text"
+        value={value}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      Icon: <input type='text'  value={icon} onChange={e => setIcon(e.target.value)}/>
+      <button>Add column</button>
+    </form>
+  );
 };
 
 export default ColumnForm;
