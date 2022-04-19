2cae19a48ede7e1d38f0e00000583447307c264c
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-19 13:21:07 +0200

    Change rendering columns based on array data and add possibility to add new column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,20 +1,44 @@
-import styles from './List.module.scss';
-import Column from '../Column/Column.js';
+import styles from "./List.module.scss";
+import Column from "../Column/Column.js";
+import { useState } from "react";
+import shortid from 'shortid';
 
 const List = () => {
-    return (
-      <div className={styles.list}>
-          <header className={styles.header}>
-              <h2 className={styles.title}>Things to do<span>soon!</span></h2>
-          </header>
-          <p className={styles.description}>Interesting things I want to check out</p>
-          <section className={styles.columns}>
-              <Column title="Books" icon="book"/>
-              <Column title="Movies" icon="film"/>
-              <Column title="Games" icon="gamepad"/>
-          </section>
-      </div>
-    );
-  };
-  
-  export default List;
+  const [columns, setColumns] = useState([
+    { id: 1, title: "Books", icon: "book" },
+    { id: 2, title: "Movies", icon: "film" },
+    { id: 3, title: "Games", icon: "gamepad" },
+  ]);
+
+  const handleSubmit = e => {
+	e.preventDefault();
+	setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+};
+
+const [value, setValue] = useState('');
+
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
+          Things to do<span>soon!</span>
+        </h2>
+      </header>
+      <p className={styles.description}>
+        Interesting things I want to check out
+      </p>
+      <section className={styles.columns}>
+        {columns.map((column) => (
+          <Column key={column.id} title={column.title} icon={column.icon} />
+        ))}
+      </section>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+        <button>Add column</button>
+      </form>
+    </div>
+  );
+};
+
+export default List;
