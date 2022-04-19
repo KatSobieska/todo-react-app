96a8501469add88326a7b7d5602d5f20e6f0f725
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-19 15:08:12 +0200

    Add cards in columns array, add card reference to section columns

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,15 +6,44 @@ import shortid from "shortid";
 
 const List = () => {
   const [columns, setColumns] = useState([
-    { id: 1, title: "Books", icon: "book" },
-    { id: 2, title: "Movies", icon: "film" },
-    { id: 3, title: "Games", icon: "gamepad" },
+    {
+      id: 1,
+      title: "Books",
+      icon: "book",
+      cards: [
+        { id: 1, title: "This is Going to Hurt" },
+        { id: 2, title: "Interpreter of Maladies" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Movies",
+      icon: "film",
+      cards: [
+        { id: 1, title: "Harry Potter" },
+        { id: 2, title: "Star Wars" },
+      ],
+    },
+    {
+      id: 3,
+      title: "Games",
+      icon: "gamepad",
+      cards: [
+        { id: 1, title: "The Witcher" },
+        { id: 2, title: "Skyrim" },
+      ],
+    },
   ]);
 
   const addColumn = (newColumn) => {
     setColumns([
       ...columns,
-      { id: shortid(), title: newColumn.title, icon: newColumn.icon },
+      {
+        id: shortid(),
+        title: newColumn.title,
+        icon: newColumn.icon,
+        cards: [],
+      },
     ]);
   };
 
@@ -30,7 +59,13 @@ const List = () => {
       </p>
       <section className={styles.columns}>
         {columns.map((column) => (
-          <Column key={column.id} title={column.title} icon={column.icon} />
+          <Column
+            key={column.id}
+            id={column.id}
+            title={column.title}
+            icon={column.icon}
+            cards={column.cards}
+          />
         ))}
       </section>
       <ColumnForm action={addColumn} />
