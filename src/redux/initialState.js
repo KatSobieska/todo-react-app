66ae83868764ee8e732d1aab684e35335d66ac28
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-30 10:35:21 +0200

    Correct columnId for cards

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -19,11 +19,11 @@ const initialState = {
 
   cards: [
     { id: 1, columnId: 1, title: "This is Going to Hurt" },
-    { id: 2, columnId: 2, title: "Interpreter of Maladies" },
-    { id: 3, columnId: 3, title: "Harry Potter" },
-    { id: 4, columnId: 4, title: "Star Wars" },
-    { id: 5, columnId: 5, title: "The Witcher" },
-    { id: 6, columnId: 6, title: "Skyrim" },
+    { id: 2, columnId: 1, title: "Interpreter of Maladies" },
+    { id: 3, columnId: 2, title: "Harry Potter" },
+    { id: 4, columnId: 2, title: "Star Wars" },
+    { id: 5, columnId: 3, title: "The Witcher" },
+    { id: 6, columnId: 3, title: "Skyrim" },
   ],
 };
 
