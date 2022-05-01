6cbc2403e0f7a38e4ba22ebf25bf252336853c4a
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-01 12:02:16 +0200

    Import function strContains and use it in getFilteredCards

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { createStore } from "redux";
 import shortid from "shortid";
 import initialState from "./initialState";
+import { strContains } from "../utils/strContains";
 
 //selectors
 export const getFilteredCards = ({ cards, searchString }, columnId) =>
   cards.filter(
     (card) =>
-      card.columnId === columnId &&
-      card.title.toLowerCase().includes(searchString.toLowerCase())
+      card.columnId === columnId && strContains(card.title, searchString)
   );
 
 const reducer = (state, action) => {
