873544ac75b71bd87db7daf6f7b64a3956d345c1
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-01 10:11:25 +0200

    Add useDispatch and useState import

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,8 @@
 import styles from "./SearchForm.module.scss";
 import TextInput from "../TextInput/TextInput.js";
 import Button from "../Button/Button.js";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 const SearchForm = () => {
   return (
