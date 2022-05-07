0b84693387f44673b32b06264483c952ac818af7
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-07 13:54:06 +0200

    Add import of PageTitle and use it in div

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,12 @@
 import styles from "./About.module.scss";
+import PageTitle from "../PageTitle/PageTitle";
 
 const About = () => {
-  return <div className={styles.title}>About</div>;
+  return (
+    <div className={styles.title}>
+      <PageTitle pageTitle="About" />
+    </div>
+  );
 };
 
 export default About;
