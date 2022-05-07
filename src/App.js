662a823313f931ba067c24b49d75392001e8427b
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-07 17:30:16 +0200

    Add List import and create new Route for List

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About/About.js";
 import Favorite from "./components/Favorite/Favorite.js";
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound/PageNotFound.js";
+import List from "./components/List/List.js";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
+          <Route path="/list/:listId" element={<List />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Container>
