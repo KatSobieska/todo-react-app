cd119d8cbc0b514180343fa5df55f28d8939a4c4
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-09 20:47:52 +0200

    Add structure for component Favorite

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,10 +1,34 @@
 import styles from "./Favorite.module.scss";
 import PageTitle from "../PageTitle/PageTitle";
+import { useSelector } from "react-redux";
+import { getFavoriteCard } from "../../redux/store";
+import Card from "../Card/Card";
 
 const Favorite = () => {
+  const favoriteCards = useSelector((state) => getFavoriteCard(state));
+  console.log("favCArds", favoriteCards);
+
+  if (favoriteCards.length === 0)
+    return (
+      <div className={styles.hero}>
+        <PageTitle title="Favorite" />
+        <p className={styles.subtitle}>UPS...No cards...</p>
+      </div>
+    );
+
   return (
-    <div className={styles.title}>
-      <PageTitle pageTitle="Favorite" />
+    <div className={styles.hero}>
+      <PageTitle title="Favorite" />
+      <ul className={styles.column}>
+        {favoriteCards.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            isFavorite={card.isFavorite}
+            cardId={card.id}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
