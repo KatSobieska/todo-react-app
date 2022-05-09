0d091e828d5310f4c7ae42d7e886b8a81b409635
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-09 20:53:21 +0200

    Add button for making card as favorite

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggleCardFavorite } from "../../redux/store";
 import styles from "./Card.module.scss";
+import clsx from "clsx";
 
 const Card = (props) => {
-  return <li className={styles.card}>{props.title}</li>;
+  const dispatch = useDispatch();
+  const [isFavorite, setIsFavorite] = useState(props.isFavorite);
+  const cardId = props.cardId;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsFavorite(!isFavorite);
+    dispatch(toggleCardFavorite(cardId));
+  };
+
+  return (
+    <li className={styles.card}>
+      {props.title}
+      <button
+        className={clsx(styles.favoriteButton, isFavorite && styles.isFavorite)}
+        onClick={handleSubmit}
+      >
+        <span className="fa fa-star-o"></span>
+      </button>
+    </li>
+  );
 };
 
 export default Card;
