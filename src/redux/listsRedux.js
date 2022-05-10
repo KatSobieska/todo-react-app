7fec85deb70f74d173f5b9a7f32e92d1eed7b3f3
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-10 21:53:57 +0200

    Add const ADD_LIST

--- src/redux/listsRedux.js
+++ src/redux/listsRedux.js
@@ -1,8 +1,10 @@
 import shortid from "shortid";
 
+const ADD_LIST = "app/lists/ADD_LIST";
+
 function listsReducer(statePart = [], action) {
   switch (action.type) {
-    case "ADD_LIST":
+    case ADD_LIST:
       return [...statePart, { ...action.payload, id: shortid() }];
     default:
       return statePart;
