cc475108d20809460919676dee683d03a9da82a9
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-10 22:04:35 +0200

    Add const ADD_COLUMN

--- src/redux/columnsRedux.js
+++ src/redux/columnsRedux.js
@@ -6,12 +6,15 @@ export const getAllColumns = (state) => state.columns;
 export const getColumnsByList = ({ columns }, listId) =>
   columns.filter((column) => column.listId === listId);
 
+// actions
+const ADD_COLUMN = "app/lists/ADD_COLUMN";
+
 // actions creators
 
-export const addColumn = (payload) => ({ type: "ADD_COLUMN", payload });
+export const addColumn = (payload) => ({ type: ADD_COLUMN, payload });
 const columnsReducer = (statePart = [], action) => {
   switch (action.type) {
-    case "ADD_COLUMN":
+    case ADD_COLUMN:
       return [...statePart, { ...action.payload, id: shortid() }];
     default:
       return statePart;
