4b7c7a9724eee4371f79f4f3fb3c5c79606f94fa
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-10 22:10:50 +0200

    Add const ADD_LIST and TOGGLE_CARD_FAVORITE

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -12,18 +12,23 @@ export const getFavoriteCard = ({ cards }) =>
   cards.filter((card) => card.isFavorite === true);
 
 // action creators
-export const addCard = (payload) => ({ type: "ADD_CARD", payload });
+export const addCard = (payload) => ({ type: ADD_CARD, payload });
 
 export const toggleCardFavorite = (payload) => ({
-  type: "TOGGLE_CARD_FAVORITE",
+  type: TOGGLE_CARD_FAVORITE,
   payload,
 });
 
+// actions
+
+const TOGGLE_CARD_FAVORITE = "app/lists/TOGGLE_CARD_FAVORITE";
+const ADD_CARD = "app/lists/ADD_CARD";
+
 const cardsReducer = (statePart = [], action) => {
   switch (action.type) {
-    case "ADD_CARD":
+    case ADD_CARD:
       return [...statePart, { ...action.payload, id: shortid() }];
-    case "TOGGLE_CARD_FAVORITE":
+    case TOGGLE_CARD_FAVORITE:
       return statePart.map((card) =>
         card.id === action.payload
           ? { ...card, isFavorite: !card.isFavorite }
