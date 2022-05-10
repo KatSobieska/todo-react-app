4db217b5798c2bcf4a622cf7d1be360b9a543790
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-10 22:11:43 +0200

    Correct import of toggleCardFavorite

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { toggleCardFavorite } from "../../redux/store";
+import { toggleCardFavorite } from "../../redux/cardsRedux";
 import styles from "./Card.module.scss";
 import clsx from "clsx";
 
